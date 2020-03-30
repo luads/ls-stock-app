a6deded3901884ad61513d8b7ca0f0c3efd68e7e
Luã de Souza <lua.desouza@theiconic.com.au>
2020-03-30 19:50:15 +1100

    Create an exception listener to return proper HTTP codes

---
+++ backend/src/EventListener/ApiExceptionSubscriber.php
@@ -0,0 +1,30 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\EventListener;
+
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Event\ExceptionEvent;
+use Symfony\Component\HttpKernel\KernelEvents;
+
+class ApiExceptionSubscriber implements EventSubscriberInterface
+{
+    public function onKernelException(ExceptionEvent $event): void
+    {
+        $exception = $event->getThrowable();
+
+        $response = new JsonResponse(['message' => $exception->getMessage()], Response::HTTP_BAD_REQUEST);
+
+        $event->setResponse($response);
+    }
+
+    public static function getSubscribedEvents(): array
+    {
+        return [
+            KernelEvents::EXCEPTION => 'onKernelException',
+        ];
+    }
+}
