718cf0f4b077cd46e790a4c96de10be61d2ebc02
Luã de Souza <lua.desouza@theiconic.com.au>
2020-03-31 00:45:16 +1100

    Use HTTP 429 for rate limited exceptions

--- backend/src/EventListener/ApiExceptionSubscriber.php
+++ backend/src/EventListener/ApiExceptionSubscriber.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace App\EventListener;
 
+use App\Share\Exception\RateLimitedException;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
@@ -15,8 +16,13 @@ class ApiExceptionSubscriber implements EventSubscriberInterface
     public function onKernelException(ExceptionEvent $event): void
     {
         $exception = $event->getThrowable();
+        $statusCode = Response::HTTP_BAD_REQUEST;
 
-        $response = new JsonResponse(['message' => $exception->getMessage()], Response::HTTP_BAD_REQUEST);
+        if ($exception instanceof RateLimitedException) {
+            $statusCode = Response::HTTP_TOO_MANY_REQUESTS;
+        }
+
+        $response = new JsonResponse(['message' => $exception->getMessage()], $statusCode);
 
         $event->setResponse($response);
     }
